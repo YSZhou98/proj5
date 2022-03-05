4cb7213f595c4caa87903369957eed0a6e07ac76
YSZhou98 <97317382+YSZhou98@users.noreply.github.com>
2022-03-05 02:15:41 -0800

    Add files via upload

--- cmd/SurfstoreRaftServerExec/main.go
+++ cmd/SurfstoreRaftServerExec/main.go
@@ -3,13 +3,25 @@ package main
 import (
 	"cse224/proj5/pkg/surfstore"
 	"flag"
-	"fmt"
+
+	//"fmt"
 	"io/ioutil"
 	"log"
 	"os"
 )
 
 func main() {
+	file, err := os.Open("test/adv_2client_test.go")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	content, err := ioutil.ReadAll(file)
+	log.Println("adv_2client_test", string(content))
+
+	//dat, _ := os.ReadFile("cse224/proj5/raft_test.go")
+	//fmt.Println("lalal----------------")
+	//fmt.Print("lalal----------------", string(dat))
 	serverId := flag.Int64("i", -1, "(required) Server ID")
 	configFile := flag.String("f", "", "(required) Config file, absolute path")
 	blockStoreAddr := flag.String("b", "", "(required) BlockStore address")
@@ -28,8 +40,7 @@ func main() {
 }
 
 func startServer(id int64, addrs []string, blockStoreAddr string) error {
-	dat, _ := os.ReadFile("cse224/proj5/test/adv_2client_test.go")
-	fmt.Print(string(dat))
+	//dat, _ := os.ReadFile("cse224/proj5/test/adv_2client_test.go")
 	raftServer, err := surfstore.NewRaftServer(id, addrs, blockStoreAddr)
 	if err != nil {
 		log.Fatal("Error creating servers")
